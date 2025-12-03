5c4bcd88e32e695cacd96af3bd1b861e003aae32
MuhElismail <muhelismail@gmail.com>
2025-12-03 14:00:50 +0300

    update architecture

--- src/ECom.Application/Products/GetProductByIdHandler.cs
+++ src/ECom.Application/Products/GetProductByIdHandler.cs
@@ -1,12 +1,10 @@
 ﻿namespace ECom.Application.Products;
 
-public class GetProductByIdHandler
+public class GetProductByIdHandler(IProductRepository repo)
 {
-    private readonly IProductRepository _repo;
-    public GetProductByIdHandler(IProductRepository repo) => _repo = repo;
     public async Task<ProductDto?> Handle(GetProductByIdQuery q, CancellationToken ct)
     {
-        var p = await _repo.GetByIdAsync(q.Id, ct);
+        var p = await repo.GetByIdAsync(q.Id, ct);
         if (p == null) return null;
         return new ProductDto(p.Id, p.SKU, p.Name, p.Price, p.Stock);
     }
